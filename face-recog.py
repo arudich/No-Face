b5e450acb54e06c53871e2c2caf7e23e0e147345
Avi Rudich <avi.rudich@gmail.com>
2020-02-10 11:20:05 -0500

    simple face detector network running

--- face-recog.py
+++ face-recog.py
@@ -10,30 +10,33 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-img = cv2.imread("person.jpg")
+img = cv2.imread("two_people.jpg")
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 alignment = Align.AlignDlib('TrainedNN/models/landmarks.dat')
-face = alignment.getLargestFaceBoundingBox(img)
-x = face.left()
-y = face.top()
-w = face.width()
-h = face.height()
-img1 = cv2.rectangle(img ,(x,y),(x+w,y+h),(255, 0, 0), 2)
-plt.subplot(131)
-plt.imshow(img)
-plt.subplot(132)
-plt.imshow(img1)
-plt.subplot(133)
-crop_img = img1[y:y+h, x:x+w]
-#print(np.array(crop_img).shape)
-#plt.imshow(crop_img)
+#face = alignment.getLargestFaceBoundingBox(img)
+faces = alignment.getAllFaceBoundingBoxes(img)
 
-face_aligned = alignment.align(96, img, alignment.getLargestFaceBoundingBox(img), landmarkIndices=Align.AlignDlib.OUTER_EYES_AND_NOSE)
-plt.imshow(face_aligned)
-print(face_aligned.shape)
-#plt.show()
+for face in faces:
+    x = face.left()
+    y = face.top()
+    w = face.width()
+    h = face.height()
+    img1 = cv2.rectangle(img ,(x,y),(x+w,y+h),(255, 0, 0), 2)
+    plt.subplot(131)
+    plt.imshow(img)
+    plt.subplot(132)
+    plt.imshow(img1)
+    plt.subplot(133)
+    crop_img = img1[y:y+h, x:x+w]
+    #print(np.array(crop_img).shape)
+    #plt.imshow(crop_img)
 
+    face_aligned = alignment.align(96, img, alignment.getLargestFaceBoundingBox(img), landmarkIndices=Align.AlignDlib.OUTER_EYES_AND_NOSE)
+    plt.imshow(face_aligned)
+    print(face_aligned.shape)
+    plt.show()
 
+exit(0)
 model = m.create_model()
 model.load_weights('TrainedNN/open_face.h5')
 
