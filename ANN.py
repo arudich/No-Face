09c43f87cf7be67d9bd28d55bfa169d1f650b877
Avi Rudich <avi.rudich@gmail.com>
2020-04-01 15:15:00 -0400

    pushing trainedNN again

--- ANN.py
+++ ANN.py
@@ -27,15 +27,50 @@ class ANN:
         self.arch_num = arch_num
         self.model = self.init_arch(arch_num)
 
-        
-        self.sess = tf.Session()
-        init = tf.global_variables_initializer()
+        fc = FaceClassifier()
+        self.init_classifier_gradients(fc)
+        self.init_model_grads(fc)
+
+        self.Adam = tf.compat.v1.train.AdamOptimizer(learning_rate = .001)
+        self.update = self.Adam.apply_gradients(zip(self.model_trainer,self.model.trainable_weights))
+
+        self.sess = tf.compat.v1.Session()
+        init = tf.compat.v1.global_variables_initializer()
         self.sess.run(init)
-        K.set_session(self.sess)
+
+        fc.load_models()
+
+        img = fc.bound_img('./images/avi/avi_0.JPG')
+        imgs = np.array([img])
+        projection = self.predict_imgs(imgs)
+
+        yTrue = np.zeros(fc.num_people)
+        yTrue[2] = 1
+        yTrue = np.array([yTrue])
+
+        feed_dict = {}
+        fc.forward_pass(img,projection,yTrue,feed_dict)
+        feed_dict[self.model.input] = imgs
+
+        print("Loss: ")
+        self.sess.run(tf.print(fc.loss),feed_dict=feed_dict)
+        self.sess.run(self.update,feed_dict=feed_dict)
+        print("Loss: ")
+        self.sess.run(tf.print(fc.loss),feed_dict=feed_dict)
+        self.sess.run(self.update,feed_dict=feed_dict)
+        print("Loss: ")
+        self.sess.run(tf.print(fc.loss),feed_dict=feed_dict)
+        self.sess.run(self.update,feed_dict=feed_dict)
+        print("Loss: ")
+        self.sess.run(tf.print(fc.loss),feed_dict=feed_dict)
+        
 
     def custom_loss_function(self,y_actual,y_pred):
         return 0
 
+    def init_model_grads(self,fc):
+        self.model_trainer = tf.gradients(self.model.output,
+            self.model.trainable_weights,grad_ys = self.projection_grads)
 
     def init_arch(self,arch_num):
         if arch_num == 1:
@@ -44,27 +79,33 @@ class ANN:
             print("No architecture with that number...using architecture 1")
             return self.arch_1()
 
+    def predict_imgs(self,imgs):
+        return self.model.predict(imgs)
+
+    def predict_img(self,img):
+        return self.model.predict(np.array([img]))[0]
+
     def arch_1(self):
-        print("Architecture 1 init:")
+        #print("Architecture 1 init:")
         myInput = Input(shape=(96, 96, 3))
 
         x = ZeroPadding2D(padding=(3, 3), input_shape=(96, 96, 3))(myInput)
         x = Conv2D(64, (7, 7), strides=(1, 1), name='conv1')(x) 
-        x = BatchNormalization(axis=3, epsilon=0.00001, name='bn3')(x)
+        x = BatchNormalization(axis=3, epsilon=0.00001, name='bn1')(x)
         # 64 7x7 convos
         #shape = (96,96,64)   
-        print("Conv 1 output shape = " + str(np.shape(x))) 
+        #print("Conv 1 output shape = " + str(np.shape(x))) 
         y = ZeroPadding2D(padding = (2,2))(x)
         y = Conv2D(64, (5 , 5), strides = (1,1))(y)
-        y = BatchNormalization(axis=3, epsilon=0.00001, name='bn3')(y)
+        y = BatchNormalization(axis=3, epsilon=0.00001, name='bn2')(y)
         #shape = (96,96,64) 
-        print("Conv 2 output shape = " + str(np.shape(y))) 
+        #print("Conv 2 output shape = " + str(np.shape(y))) 
 
         z = ZeroPadding2D(padding = (1,1))(y)
         z = Conv2D(64, (3 , 3), strides = (1,1))(z)
         z = BatchNormalization(axis=3, epsilon=0.00001, name='bn3')(z)
         #shape = (96,96,16)
-        print("Conv 3 output shape = " + str(np.shape(z))) 
+        #print("Conv 3 output shape = " + str(np.shape(z))) 
 
         inception_3a_3x3 = Conv2D(96, (1, 1), name='inception_3a_3x3_conv1')(z)
         inception_3a_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_3x3_bn1')(inception_3a_3x3)
@@ -88,7 +129,7 @@ class ANN:
 
         inception_3a = concatenate([inception_3a_3x3, inception_3a_5x5, inception_3a_1x1], axis=3)
         #shape = (96,96,224)
-        print("Inception 3a shape: " + str(np.shape(inception_3a)))
+        #print("Inception 3a shape: " + str(np.shape(inception_3a)))
 
         # Inception3b
         inception_3b_3x3 = Conv2D(96, (1, 1), name='inception_3b_3x3_conv1')(inception_3a)
@@ -112,17 +153,29 @@ class ANN:
         inception_3b_1x1 = Activation('relu')(inception_3b_1x1)
 
         inception_3b = concatenate([inception_3b_3x3, inception_3b_5x5, inception_3b_1x1], axis=3)
-        print("Inception 3b shape: " + str(np.shape(inception_3b)))
+        #print("Inception 3b shape: " + str(np.shape(inception_3b)))
 
         O = layers.Dense(3, activation='relu', kernel_regularizer=l2(0.0001))(inception_3b)
-        print("Output shape: " + str(np.shape(O)))
+        #print("Output shape: " + str(np.shape(O)))
 
-        model = Model(inputs=I,outputs=O)
-        model.compile("Adam",
-            loss=self.custom_loss_function)
+        model = Model(inputs=myInput,outputs=O)
+        # model.compile("Adam",
+        #     loss=self.custom_loss_function)
         return model
 
-
+    def init_classifier_gradients(self,fc):
+        self.yTrue = tf.compat.v1.placeholder(tf.float32)
+        self.loss = tf.losses.MSE(self.yTrue,fc.classifier.output)
+        self.loss_grads = tf.gradients(self.loss,fc.classifier.output)
+        self.classifier_grads = tf.gradients(fc.classifier.output,
+            fc.classifier.input,grad_ys = self.loss_grads)
+        self.classifier_trainer = tf.gradients(fc.classifier.output,
+            fc.classifier.trainable_weights,grad_ys = self.loss_grads)
+        self.OF_grads = tf.gradients(fc.OF.output,fc.OF.input,grad_ys = self.classifier_grads)
+        self.img = tf.compat.v1.placeholder(tf.float32,shape=(None,96,96,3))
+        self.projection = tf.compat.v1.placeholder(tf.float32,shape=(None,96,96,3))
+        self.sum_images = tf.math.add(self.img,self.projection)
+        self.projection_grads = tf.gradients(self.sum_images,self.projection,grad_ys = self.OF_grads)
 
 if __name__ == '__main__':
     ann = ANN(1)
--- face_classifier.py
+++ face_classifier.py
@@ -1,10 +1,15 @@
 from face_classifier_training import FaceClassifierTrainer
 import tensorflow as tf
+import keras
 from keras import layers
 from keras.models import Model
 from keras.regularizers import l2
 import keras.backend as K
 import keras.losses as losses
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate, Dense
+from keras.layers.core import Lambda, Flatten, Dense
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 import string
 import numpy as np
 import math
@@ -17,9 +22,10 @@ import TrainedNN.model as m
 import TrainedNN.Align as Align
 import TrainedNN.Data as Data
 import TrainedNN.utils as utils
+import matplotlib.pyplot as plt
 
 class FaceClassifier(FaceClassifierTrainer):
-    def __init__(self,num_people = 0):
+    def __init__(self,num_people = 0,arch_num = 1):
         self.img_dir = "./images"
         if(num_people == 0):
             self.num_people = self.count_people()
@@ -28,123 +34,102 @@ class FaceClassifier(FaceClassifierTrainer):
         else: self.num_people = num_people
         self.classifier = self.init_model(self.num_people)
         self.OF = m.create_model()
+        self.ANN = self.init_ANN_arch(arch_num)
         self.person_dict = self.read_person_dict()
 
         self.init_classifier_gradients()
+
         self.Adam = tf.compat.v1.train.AdamOptimizer(learning_rate = .001)
-        self.update = self.Adam.apply_gradients(zip(self.classifier_trainer,self.classifier.trainable_weights))
-        #self.run_classifier_gradients()
+        self.update = self.Adam.apply_gradients(zip(self.ANN_trainer,self.ANN.trainable_weights))
+        #self.Adam_2 = tf.compat.v1.train.AdamOptimizer(learning_rate = .00025)
+        #self.update_2 = self.Adam_2.apply_gradients(zip(self.proj_grads,self.ANN.trainable_weights))
 
         self.sess = tf.compat.v1.Session()
         init = tf.compat.v1.global_variables_initializer()
         self.sess.run(init)
-        #tf.compat.v1.keras.backend.set_session(self.sess)
-        #K.set_session(self.sess)
-        self.load_model(self.classifier)
-        self.OF.load_weights('TrainedNN/open_face.h5')
+        tf.compat.v1.keras.backend.set_session(self.sess)
+
+        self.load_models()
 
         img = self.bound_img('./images/avi/avi_0.JPG')
-        img = np.array([img])
-        projection = np.random.uniform(size=(1,96,96,3))
-        test = self.predict_bounded_face((img+projection)[0])
+        imgs = np.array([img])
 
         yTrue = np.zeros(self.num_people)
         yTrue[2] = 1
         yTrue = np.array([yTrue])
 
         feed_dict = {}
-        self.forward_pass(img,projection,yTrue,feed_dict)
-        '''
-        print("Projection: ")
-        self.sess.run(tf.print(self.projection),feed_dict=feed_dict)
-        print("Image: ")
-        self.sess.run(tf.print(self.img),feed_dict=feed_dict)
-        print("Combination: ")
-        self.sess.run(tf.print(self.sum_images),feed_dict=feed_dict)
-        print("Loss: ")
-        self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
-        print("loss_grads: ")
-        self.sess.run(tf.print(self.loss_grads),feed_dict=feed_dict)
-        print("classifier_grads: ")
-        self.sess.run(tf.print(self.classifier_grads),feed_dict=feed_dict)
-        print("OF_grads: ")
-        self.sess.run(tf.print(self.OF_grads),feed_dict=feed_dict)
-        print("Projection_grads: ")
-        self.sess.run(tf.print(self.projection_grads),feed_dict=feed_dict)
-        '''
-        print("Loss: ")
-        self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
-        self.sess.run(tf.print(self.classifier_trainer),feed_dict=feed_dict)
-        print("------------------------------------------------------------")
-        self.sess.run(tf.print(self.classifier.trainable_weights),feed_dict=feed_dict)
-        self.sess.run(self.update,feed_dict=feed_dict)
-        print("Loss: ")
-        self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
-        self.sess.run(self.update,feed_dict=feed_dict)
-        print("Loss: ")
-        self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
-        self.sess.run(self.update,feed_dict=feed_dict)
-        print("Loss: ")
-        self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
+        self.forward_pass(imgs,yTrue,feed_dict)
+
+        for i in range(10):
+            feed_dict = {}
+            self.forward_pass(imgs,yTrue,feed_dict)
+            self.sess.run(self.update,feed_dict=feed_dict)
+            print("Loss: ")
+            self.sess.run(tf.print(self.loss),feed_dict=feed_dict)
+            print("Norm: ")
+            self.sess.run(tf.print(self.proj_size),feed_dict=feed_dict)
+            #self.sess.run(self.update_2,feed_dict=feed_dict)
+
+        plt.subplot(1,3,1)
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.subplot(1,3,2)
+        plt.imshow(cv2.cvtColor((img+self.ANN_predict_img(img))/255.0, cv2.COLOR_BGR2RGB))
+        #print(img+self.ANN_predict_img(img))
+        pred = self.predict_bounded_face(img+self.ANN_predict_img(img))
+        classification = self.identify_pred(pred)
+        print(pred)
+        print(classification)
+        plt.subplot(1,3,3)
+        plt.imshow(cv2.cvtColor((self.ANN_predict_img(img))/255.0, cv2.COLOR_BGR2RGB))
+        
+        plt.show()
+
+
 
     def init_classifier_gradients(self):
-        # actor_inputs = {
-        #     self.actor.input : states,
-        #     self.critic.get_layer('states').input : states,
-        #     self.critic.get_layer('actions').input : real_actions}
-        # critic_inputs = {
-        #     self.critic.get_layer('states').input : states,
-        #     self.critic.get_layer('actions').input : actions,
-        #     self.y_values : y_values
-        # }
-        # self.sess.run(self.updateCritic,feed_dict=critic_inputs)
-        # self.sess.run(self.updateActor, feed_dict=actor_inputs)
-
-        # self.actor_Adam = tf.train.AdamOptimizer(learning_rate = self.actor_lr)
-        # self.critic_Adam = tf.train.AdamOptimizer(learning_rate = self.critic_lr)
-
-        # self.value_grads = tf.gradients(self.critic.output, self.critic.get_layer("actions").input)
-
-        # self.param_grads = tf.gradients(
-        #     self.actor.output,self.actor.trainable_weights,grad_ys = tf.multiply(-1.,self.value_grads)) 
-
-        # self.updateActor = self.actor_Adam.apply_gradients(zip(self.param_grads,self.actor.trainable_weights))
-
-        # self.y_values = tf.placeholder(tf.float32)
-        # self.critic_loss = tf.losses.mean_squared_error(self.y_values,self.critic.outputs)
-        # self.updateCritic = self.critic_Adam.minimize(self.critic_loss,var_list = self.critic.trainable_weights)
+        self.img = tf.compat.v1.placeholder(tf.float32,shape=(None,96,96,3))
+        self.sum_images = tf.math.add(self.img,self.ANN.output)
 
         self.yTrue = tf.compat.v1.placeholder(tf.float32)
-        self.loss = tf.losses.MSE(self.yTrue,self.classifier.output)
+        self.loss = tf.multiply(-1.0,tf.losses.MSE(self.yTrue,self.classifier.output))
         self.loss_grads = tf.gradients(self.loss,self.classifier.output)
         self.classifier_grads = tf.gradients(self.classifier.output,
             self.classifier.input,grad_ys = self.loss_grads)
-        self.classifier_trainer = tf.gradients(self.classifier.output,
-            self.classifier.trainable_weights,grad_ys = self.loss_grads)
         self.OF_grads = tf.gradients(self.OF.output,self.OF.input,grad_ys = self.classifier_grads)
-        self.img = tf.compat.v1.placeholder(tf.float32,shape=(None,96,96,3))
-        self.projection = tf.compat.v1.placeholder(tf.float32,shape=(None,96,96,3))
-        self.sum_images = tf.math.add(self.img,self.projection)
-        self.projection_grads = tf.gradients(self.sum_images,self.projection,grad_ys = self.OF_grads)
+
+        self.proj_size = tf.square(1.0-tf.norm(tf.norm(self.ANN.output,axis=[1,2]),axis=1)*.0001)
+        self.proj_size_grads = tf.gradients(self.proj_size,self.ANN.output)
+        #self.ANN_mag_grads = tf.gradients(self.ANN.output,self.ANN.trainable_weights,grad_ys=self.proj_size_grads)
+
+        self.grad_sum = tf.squeeze(tf.add(self.proj_size_grads,self.OF_grads),axis=0)
+        self.ANN_trainer = tf.gradients(self.ANN.output,self.ANN.trainable_weights,grad_ys=self.grad_sum)
 
 
-    def forward_pass(self,img,projection,yTrue,feed_dict): 
+    def forward_pass(self,imgs,yTrue,feed_dict): 
         assert(np.shape(yTrue) == (1,self.num_people))
-        assert(np.shape(img) == (1,96,96,3))
-        assert(np.shape(projection) == (1,96,96,3))
+        assert(np.shape(imgs) == (1,96,96,3))
         #fills feed_dict for training step
         #returns projection_grads for training ANN
         #feed_dict = {}
-        feed_dict[self.yTrue] = yTrue
-        feed_dict[self.img] = img
-        feed_dict[self.projection] = projection
+        feed_dict[self.ANN.input] = imgs
+        projection = self.ANN_predict_imgs(imgs)
 
-        img_sum = img + projection
+        feed_dict[self.yTrue] = yTrue
+        feed_dict[self.img] = imgs
+        #feed_dict[self.projection] = projection
+
+        img_sum = imgs + projection
+        #print(img_sum)
         pred = self.OF_embed_bounded_faces(img_sum)
         feed_dict[self.OF.input] = img_sum
         feed_dict[self.classifier.input] = pred
 
-        #return feed_dict
+        return feed_dict
+
+    def load_models(self):
+        self.load_model(self.classifier)
+        self.OF.load_weights('TrainedNN/open_face.h5')
 
     def predict_face_from_cam(self,img_path):
         img = self.bound_img(img_path)
@@ -207,37 +192,97 @@ class FaceClassifier(FaceClassifierTrainer):
             person_dict[int(num)] = name
         return person_dict
 
-    def run_classifier_gradients(self):
-        optimizer = tf.compat.v1.train.AdamOptimizer()
-        yTrue = np.zeros(self.num_people)
-        yTrue[2] = 1
-        projections = tf.zeros((1,96,96,3))
-        img = self.bound_img('./images/avi/avi_0.JPG')
-        imgs = tf.convert_to_tensor(np.array([img]),dtype=tf.float32)
+    def init_ANN_arch(self,arch_num):
+        if arch_num == 1:
+            return self.ANN_arch_1()
+        else:
+            print("No architecture with that number...using architecture 1")
+            return self.ANN_arch_1()
 
-        print(self.classifier(tf.random.uniform((1,128))))
-        exit(0)
+    def ANN_predict_imgs(self,imgs):
+        return self.ANN.predict(imgs)
 
-        with tf.GradientTape() as tape:
-            #print("Projection: ",projections)
-            tape.watch(projections)
-            OF_in = projections + imgs
-            print("Combined Image: ", OF_in)
-            OF_out = self.OF(OF_in)
-            # tf.print(OF_out)
-            # print("Open Face output: ", OF_out.eval())
-            # print("Open Face output: ", tf.convert_to_tensor(OF_out).numpy())
-            #print(tape.gradient(OF_out,projections))
-            #exit(0)
-            class_out = self.classifier.predict(OF_out)
-            print("Classifier output: ", class_out)
-            loss = tf.losses.MSE(np.array([yTrue]),class_out)
-            print("Loss: ", loss)
-            exit(0)
-            grads = tape.gradient(loss, projection)
-            print(grads)
-            optimizer.apply_gradients(zip(grads, projection))
-            print("Projection after step: ",projection)
+    def ANN_predict_img(self,img):
+        return self.ANN.predict(np.array([img]))[0]
+
+    def ANN_arch_1(self):
+        #print("Architecture 1 init:")
+        myInput = Input(shape=(96, 96, 3))
+
+        x = ZeroPadding2D(padding=(3, 3), input_shape=(96, 96, 3))(myInput)
+        x = Conv2D(64, (7, 7), strides=(1, 1), name='conv1')(x) 
+        x = BatchNormalization(axis=3, epsilon=0.00001, name='bn1')(x)
+        # 64 7x7 convos
+        #shape = (96,96,64)   
+        #print("Conv 1 output shape = " + str(np.shape(x))) 
+        y = ZeroPadding2D(padding = (2,2))(x)
+        y = Conv2D(64, (5 , 5), strides = (1,1))(y)
+        y = BatchNormalization(axis=3, epsilon=0.00001, name='bn2')(y)
+        #shape = (96,96,64) 
+        #print("Conv 2 output shape = " + str(np.shape(y))) 
+
+        z = ZeroPadding2D(padding = (1,1))(y)
+        z = Conv2D(64, (3 , 3), strides = (1,1))(z)
+        z = BatchNormalization(axis=3, epsilon=0.00001, name='bn3')(z)
+        #shape = (96,96,16)
+        #print("Conv 3 output shape = " + str(np.shape(z))) 
+
+        inception_3a_3x3 = Conv2D(96, (1, 1), name='inception_3a_3x3_conv1')(z)
+        inception_3a_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_3x3_bn1')(inception_3a_3x3)
+        inception_3a_3x3 = Activation('relu')(inception_3a_3x3)
+        inception_3a_3x3 = ZeroPadding2D(padding=(1, 1))(inception_3a_3x3)
+        inception_3a_3x3 = Conv2D(128, (3, 3), name='inception_3a_3x3_conv2')(inception_3a_3x3)
+        inception_3a_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_3x3_bn2')(inception_3a_3x3)
+        inception_3a_3x3 = Activation('relu')(inception_3a_3x3)
+
+        inception_3a_5x5 = Conv2D(16, (1, 1), name='inception_3a_5x5_conv1')(z)
+        inception_3a_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_5x5_bn1')(inception_3a_5x5)
+        inception_3a_5x5 = Activation('relu')(inception_3a_5x5)
+        inception_3a_5x5 = ZeroPadding2D(padding=(2, 2))(inception_3a_5x5)
+        inception_3a_5x5 = Conv2D(32, (5, 5), name='inception_3a_5x5_conv2')(inception_3a_5x5)
+        inception_3a_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_5x5_bn2')(inception_3a_5x5)
+        inception_3a_5x5 = Activation('relu')(inception_3a_5x5)
+
+        inception_3a_1x1 = Conv2D(64, (1, 1), name='inception_3a_1x1_conv')(x)
+        inception_3a_1x1 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3a_1x1_bn')(inception_3a_1x1)
+        inception_3a_1x1 = Activation('relu')(inception_3a_1x1)
+
+        inception_3a = concatenate([inception_3a_3x3, inception_3a_5x5, inception_3a_1x1], axis=3)
+        #shape = (96,96,224)
+        #print("Inception 3a shape: " + str(np.shape(inception_3a)))
+
+        # Inception3b
+        inception_3b_3x3 = Conv2D(96, (1, 1), name='inception_3b_3x3_conv1')(inception_3a)
+        inception_3b_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_3x3_bn1')(inception_3b_3x3)
+        inception_3b_3x3 = Activation('relu')(inception_3b_3x3)
+        inception_3b_3x3 = ZeroPadding2D(padding=(1, 1))(inception_3b_3x3)
+        inception_3b_3x3 = Conv2D(32, (3, 3), name='inception_3b_3x3_conv2')(inception_3b_3x3)
+        inception_3b_3x3 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_3x3_bn2')(inception_3b_3x3)
+        inception_3b_3x3 = Activation('relu')(inception_3b_3x3)
+
+        inception_3b_5x5 = Conv2D(32, (1, 1), name='inception_3b_5x5_conv1')(inception_3a)
+        inception_3b_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_5x5_bn1')(inception_3b_5x5)
+        inception_3b_5x5 = Activation('relu')(inception_3b_5x5)
+        inception_3b_5x5 = ZeroPadding2D(padding=(2, 2))(inception_3b_5x5)
+        inception_3b_5x5 = Conv2D(16, (5, 5), name='inception_3b_5x5_conv2')(inception_3b_5x5)
+        inception_3b_5x5 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_5x5_bn2')(inception_3b_5x5)
+        inception_3b_5x5 = Activation('relu')(inception_3b_5x5)
+
+        inception_3b_1x1 = Conv2D(16, (1, 1), name='inception_3b_1x1_conv')(inception_3a)
+        inception_3b_1x1 = BatchNormalization(axis=3, epsilon=0.00001, name='inception_3b_1x1_bn')(inception_3b_1x1)
+        inception_3b_1x1 = Activation('relu')(inception_3b_1x1)
+
+        inception_3b = concatenate([inception_3b_3x3, inception_3b_5x5, inception_3b_1x1], axis=3)
+        #print("Inception 3b shape: " + str(np.shape(inception_3b)))
+        #norm_O = BatchNormalization(axis=3, epsilon=0.00001, name='norm_out')(inception_3b)
+
+        O = layers.Dense(3, activation='relu', kernel_regularizer=l2(0.0001))(inception_3b)
+        #print("Output shape: " + str(np.shape(O)))
+
+        model = Model(inputs=myInput,outputs=O)
+        # model.compile("Adam",
+        #     loss=self.custom_loss_function)
+        return model
 
 if __name__ == '__main__':
     FC = FaceClassifier()
