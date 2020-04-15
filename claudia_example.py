b7fb7bbd66708b1b9be117f6232ba49be78d1d1e
Claudia Kho <ckho@andrew.cmu.edu>
2020-04-15 11:54:07 -0400

    added modification through dlib

--- claudia_example.py
+++ claudia_example.py
@@ -3,7 +3,50 @@ from claudia_face_classifier import FaceClassifier
 
 if __name__ == '__main__':
     fc = FaceClassifier()
-    fc.predict_face_from_cam('./images/avi/avi_0.JPG')
+
+    ### Random shapes
+    ### Darken nose area
+    ### Eyebrow shape
+
+    ### Lip color ###
+    # fc.predict_face_from_cam('./images/claudia/claudia_4.JPG')
+    # fc.predict_face_from_cam('./images/claudia/claudia_edited_lip_2.JPG')
+
+    # recognizes dtom as avi!
+    # fc.predict_face_from_cam('./images/dtom/dtom_83.JPG')
+    # fc.predict_face_from_cam('./images/dtom/dtom_edited_lip.JPG')
+    # fc.predict_face_from_cam('./images/dtom/dtom_4.JPG')
+    # fc.predict_face_from_cam('./images/dtom/dtom_edited_lip_2.JPG')
+
+    ### Moustache ###
+    # fc.predict_face_from_cam('./images/claudia/claudia_72.JPG')
+    # fc.predict_face_from_cam('./images/claudia/claudia_edited_facialhair.JPG')
+    # fc.predict_face_from_cam('./images/aggie/aggie_97.JPG')
+    # fc.predict_face_from_cam('./images/aggie/aggie_edited_facialhair.JPG')
+
+    # probability of dtom increased
+    # fc.predict_face_from_cam('./images/dtom/dtom_64.JPG')
+    # fc.predict_face_from_cam('./images/dtom/dtom_edited_facialhair.JPG')
+
+    # fc.predict_face_from_cam('./images/avi/avi_91.JPG')
+    # fc.predict_face_from_cam('./images/avi/avi_edited_facialhair.JPG')
+
+    ### Side facial hair ###
+    # probability of avi increased
+    # fc.predict_face_from_cam('./images/avi/avi_3.JPG')
+    # fc.predict_face_from_cam('./images/avi/avi_edited_lighten.JPG')
+
+    # fc.predict_face_from_cam('./images/aggie/aggie_1.JPG')
+    # fc.predict_face_from_cam('./images/aggie/aggie_edited_lighten.JPG')
+
+    # probability of aggie increased
+    # fc.predict_face_from_cam('./images/dtom/dtom_24.JPG')
+    # fc.predict_face_from_cam('./images/dtom/dtom_edited_lighten.JPG')
+
+    ### Nose color ###
+    # fc.predict_face_from_cam('./images/claudia/claudia_10.JPG')
+    fc.predict_face_from_cam('./images/claudia/claudia_edited_nose.JPG')
+
 
     #This method runs a couple others
     '''
@@ -24,5 +67,5 @@ if __name__ == '__main__':
     #it outputs probabilities
     #identify_pred(probabilities) maps it to the name of the person
 
-    #look at ./images/img_key.txt to see who the indeces correspond to 
+    #look at ./images/img_key.txt to see who the indeces correspond to
     #in the output probabilities
---
+++ modify.py
@@ -0,0 +1,87 @@
+import sys
+import cv2
+import dlib
+import numpy as np
+from skimage import io
+
+import imutils
+from imutils import face_utils
+from imutils.face_utils import FACIAL_LANDMARKS_IDXS
+
+
+"""
+    Example usage: python modify.py images/claudia/claudia_10.jpg
+                   images/claudia/claudia_edited_nose.jpg nose
+"""
+
+
+"""
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+    ("mouth", (48, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 35)),
+    ("jaw", (0, 17))
+])
+"""
+
+
+# in BGR, not RGB!!
+COLORS = [(128, 84, 231)]
+
+
+def get_landmarks(image_file):
+    """ Gets array of facial landmarks
+    """
+
+    face_detector = dlib.get_frontal_face_detector()
+    landmark_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    image = cv2.imread(image_file)
+    # image = imutils.resize(image, width=500)
+
+    detected_faces = face_detector(image, 1)
+
+    landmarks = landmark_predictor(image, detected_faces[0])
+    # Convert landmarks to numpy array
+    landmarks_arr = face_utils.shape_to_np(landmarks)
+
+    return landmarks_arr, image
+
+
+def add_color(image, landmarks_arr, feature):
+    """ Changes the color of a given feature eg. mouth, nose
+    """
+
+    overlay = image.copy()
+    output = image.copy()
+
+    (j, k) = FACIAL_LANDMARKS_IDXS[feature]
+    pts = landmarks_arr[j:k]
+    hull = cv2.convexHull(pts)
+    cv2.drawContours(overlay, [hull], -1, COLORS[0], -1)
+
+    cv2.addWeighted(overlay, 0.75, output, 1 - 0.75, 0, output)
+
+    return output
+
+
+def main():
+    image_file = sys.argv[1]
+    output_file = sys.argv[2]
+    facial_feature = sys.argv[3]
+
+    landmarks_arr, image = get_landmarks(image_file)
+
+    # Choose modification
+    output = add_color(image, landmarks_arr, facial_feature)
+
+    cv2.imshow("Image", output)
+    cv2.waitKey(0)
+    cv2.imwrite(output_file, output)
+
+
+if __name__ == "__main__":
+    main()
